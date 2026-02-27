1289a9063b9900686ad7964e77d2b84fef29f5d8
NoahNrt <Nrtkonto@gmail.com>
2026-02-27 19:14:24 +0100

    adding a Task

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+This is my  TODO list. Just for practice.
+v0.0.0.1
+'''
+# Task could propebly be an object, because i dont want to make those manuelly for each task.
+
+class task():
+    
+
+task1 = input()
+print("Task: " + task1)
