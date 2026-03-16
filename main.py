addac425f30f9f47586d4c2451722c8aa3877697
NoahNrt <nrtkonto@gmail.com>
2026-03-16 23:56:54 +0100

    added a ID and Status check

--- main.py
+++ main.py
@@ -7,15 +7,28 @@ v0.0.0.1
 
 
 class Task:
-    def __init__(self, title, description, id): # if ready add some Timestamp, priority and tags.
+
+    id_counter = 0
+
+    def __init__ (self, title, description): # if ready add some Timestamp, priority and tags.
+        Task.id_counter += 1
+        self.id = Task.id_counter
         self.title = title
         self.description = description
-        self.id = id
 
-    def status(self):
+    def update (self, title=None, description=None): # Updater for updating a tasks values
+        if title is not None:
+            self.title = title
+        if description is not None:
+            self.description = description
+        # I think u will not edit a fcking id.. How dare you! 
+
+    def status (self): # For getting a status
         print(f"{self.title} {self.description} {self.id}")
 
-aufgabe1 = Task("Jesus", "Bitte bring mich um", 0)
+    def __str__ (self): # just for the ID - I will maybe transform this to a UUID but shorted one :D
+        return f"{self.id}"
 
+
+aufgabe1 = Task("Jesus", "Bitte bring mich um")
 aufgabe1.status()
-
